0b52c1861a1a5779b8005b4e64614f0aadb44132
SBars <samuelbarsuhn@gmail.com>
2022-12-22 13:21:46 -0600

    cleaned up code and made it actually work

--- script.js
+++ script.js
@@ -1,59 +1,71 @@
+const lowercase = "abcdefghijklmnopqrstuvwxyz";
+const uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+const numbers = "1234567890";
+const symbols = "!@#$%^&*()_=-+[]{}<>,./?";
+var newstring = "";
+
 // Assignment code here
-// STUFF I ADDED
-const lowercase = "abcdefghijklmnopqrstuvwxyz" ;
-const uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ" ;
-const numbers = "1234567890" ;
-const symbols = "!@#$%^&*()_=-+[]{}" ;
+
+function generatePassword() {
+  var answer5 = prompt("How many characters do you want your password to be?");
+  if (answer5 <= 7 || answer5 >= 129) {
+    alert("Your password must be between 8 and 128 characters long");
+    generatePassword();
+  } else {
+    var answer1 = confirm("Do you want lowercase letters in your password?");
+    var answer2 = confirm("Do you want uppercase letters in your password?");
+    var answer3 = confirm("Do you want numbers in your password?");
+    var answer4 = confirm("Do you want symbols in your password?");
+
+    if (
+      answer1 != true &&
+      answer2 != true &&
+      answer3 != true &&
+      answer4 != true
+    ) {
+      alert(
+        "You must have at leaast one character type to generate a password."
+      );
+      generatePassword();
+    }
+
+    // let password ="";
+
+    if (answer1 === true) {
+      newstring = newstring + lowercase;
+    }
+
+    if (answer2 === true) {
+      newstring = newstring + uppercase;
+    }
+
+    if (answer3 === true) {
+      newstring = newstring + numbers;
+    }
+
+    if (answer4 === true) {
+      newstring = newstring + symbols;
+    }
+    var password = "";
+
+    for (var index = 0; index < answer5; index++) {
+      var result = newstring[Math.floor(Math.random() * newstring.length)];
+
+      password = password + result;
+    }
+    return password;
+  }
+}
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
-  let answer1 = prompt("Do you want lowercase letters in your password?");
-
-  let answer2 = prompt("Do you want uppercase letters in your password?");
-
-  let answer3 = prompt("Do you want numbers in your password?");
-  
-  let answer4 = prompt("Do you want symbols in your password?");
-  
-
-
 // Write password to the #password input
 function writePassword() {
-  // var password = generatePassword();
+  var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
-
 }
-
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
-// STUFF I ADDED
-function randomLowercase() {
-  if (answer1 === "yes")
-  return lowercase[Math.floor(Math.random() * lowercase.length)];
-
-}
-console.log(randomLowercase());
-
-
-function randomUppercase() {
-  if (answer2 === "yes")
-  return uppercase[Math.floor(Math.random() * uppercase.length)];
-}
-console.log(randomUppercase());
-
-
-function randomNumber() {
-  if (answer3 === "yes")
-  return numbers[Math.floor(Math.random() * numbers.length)];
-}
-console.log(randomNumber());
-
-
-function randomSymbol() {
-  if (answer4 === "yes")
-  return symbols[Math.floor(Math.random() * symbols.length)];
-}
-console.log(randomSymbol());
