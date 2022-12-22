cb8f5f469d783424af67f16570943c28d330b2bb
SBars <samuelbarsuhn@gmail.com>
2022-12-22 13:37:40 -0600

    added comments about each proccess

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// These are the starting strings that the script will pick from to add to "newstring"
 const lowercase = "abcdefghijklmnopqrstuvwxyz";
 const uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 const numbers = "1234567890";
@@ -5,7 +6,7 @@ const symbols = "!@#$%^&*()_=-+[]{}<>,./?";
 var newstring = "";
 
 // Assignment code here
-
+// This function starts by asking how many and what type of characters you want. It will also revert the user back to the beginning if they chose options outside of the parameters
 function generatePassword() {
   var answer5 = prompt("How many characters do you want your password to be?");
   if (answer5 <= 7 || answer5 >= 129) {
@@ -29,8 +30,7 @@ function generatePassword() {
       generatePassword();
     }
 
-    // let password ="";
-
+   // This section checks to see if you picked a certain character type and then adds it to the new string that the password will be generated from.
     if (answer1 === true) {
       newstring = newstring + lowercase;
     }
@@ -47,7 +47,7 @@ function generatePassword() {
       newstring = newstring + symbols;
     }
     var password = "";
-
+ // This section will count up to the number of characters the user chose whiile adding a random character from the new string to generate the password.
     for (var index = 0; index < answer5; index++) {
       var result = newstring[Math.floor(Math.random() * newstring.length)];
 
